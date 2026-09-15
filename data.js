25c20a975fbabb26997a3a8518a32b551d8b6c39
Derek <adamtsai0408@gmail.com>
2026-09-15 23:12:36 +0800

    update

--- data.js
+++ data.js
@@ -191,7 +191,7 @@ const trip = {
           name: "離開環球影城",
           note: "",
           expand:
-            "1. 搭乘 JR 夢咲線（櫻島線）至 西九條站下車\n\n2. 西九條站轉乘 JR 大阪環狀線（往內回方向，經大正、弁天町）至 新今宮站"
+            "1. 搭乘 JR 夢咲線（櫻島線）至 西九條站下車\n\n2. 西九條站轉乘 JR 大阪環狀線（往內回方向，經大正、弁天町）至 新今宮站\n\n3. 走路10分鐘回飯店"
         },
         {
           time: "21:30",
@@ -215,21 +215,42 @@ const trip = {
     {
       date: "10/4",
       wd: "Sun",
-      title: "京都",
+      title: "京都・清水寺・祇園",
       tag: "Day 4",
       items: [
-        { time: "09:00", icon: "🏯", name: "前往京都", note: "", expand: "" }
+        { time: "07:30", icon: "🚃", name: "出門", note: "" },
+        { time: "08:45", icon: "🏯", name: "抵達清水寺", note: "", hi: true },
+        { time: "11:00", icon: "🎋", name: "三年坂（產寧坂）→ 二年坂", note: "" },
+        { time: "12:30", icon: "🍱", name: "中餐", note: "" },
+        { time: "14:00", icon: "⛩️", name: "八坂神社", note: "", hi: true },
+        { time: "15:30", icon: "🌸", name: "祇園、花見小路 → 鴨川傍晚散步", note: "" },
+        { time: "19:00", icon: "🚃", name: "返程回大國町", note: "" }
       ]
     },
     {
       date: "10/5",
       wd: "Mon",
-      title: "大阪城・通天閣・勝尾寺",
+      title: "勝尾寺・大阪城・通天閣",
       tag: "Day 5",
       items: [
-        { time: "09:00", icon: "🏯", name: "大阪城", note: "", hi: true },
-        { time: "13:00", icon: "🗼", name: "通天閣", note: "" },
-        { time: "16:00", icon: "⛩️", name: "勝尾寺", note: "", hi: true }
+        {
+          time: "08:50",
+          icon: "🚃",
+          name: "集合",
+          note: "箕面萱野站",
+          expand:
+            "1. 出發站：大國町站 \n\n2. 搭乘路線：Osaka Metro 御堂筋線（「箕面萱野」方向）\n\n3. 下車站：箕面萱野站（車程約 37 分鐘）"
+        },
+        { time: "09:20", icon: "⛩️", name: "抵達勝尾寺", note: "", hi: true },
+        {
+          time: "12:45",
+          icon: "🍖",
+          name: "燒肉但馬屋 梅田店",
+          note: "",
+          mapUrl: "https://maps.app.goo.gl/N27sZgnnZwqVPX9Z6"
+        },
+        { time: "13:00", icon: "🏯", name: "大阪城", note: "", hi: true },
+        { time: "16:30", icon: "🗼", name: "通天閣", note: "" }
       ]
     },
     {
@@ -239,8 +260,14 @@ const trip = {
       tag: "Day 6",
       items: [
         { time: "06:30", icon: "🏨", name: "飯店 Check-out", note: "退房・整理行李" },
-        { time: "07:00", icon: "🚃", name: "前往關西機場", note: "南海電鐵 Rapi:t 特急 · 難波出發 · 約 45 分", expand: "" },
-        { time: "08:00", icon: "🛫", name: "抵達關西機場", note: "辦理登機・行李托運", hi: true },
+        {
+          time: "07:30",
+          icon: "🚃",
+          name: "前往關西機場",
+          note: "南海電鐵 Rapi:t 特急 · 難波出發 · 約 45 分",
+          expand: ""
+        },
+        { time: "08:45", icon: "🛫", name: "抵達關西機場", note: "辦理登機・行李托運", hi: true },
         { time: "10:45", icon: "✈️", name: "起飛回台", note: "MM031 · 再見，大阪" }
       ]
     }
